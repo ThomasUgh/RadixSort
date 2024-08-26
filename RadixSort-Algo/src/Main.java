0404331ca339e8eb86d7b7d3cc04d803cf0f6c6d
Thomas U. <Info@ThomasU.dev>
2024-08-26 22:38:15 +0200

    Add Input String & Output

--- RadixSort-Algo/src/Main.java
+++ RadixSort-Algo/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String input = "a1B2c3A4D5e6F7g8H9i0J";
+
+        RadixSorter sorter = new RadixSorter();
+
+        System.out.println("Original String: " + input);
+        System.out.println("Sorted String: " + Output);
     }
 }
